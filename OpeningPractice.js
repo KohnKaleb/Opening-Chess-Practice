83b2b9a134b7214c4475a287ac18a023e95e9d13
Kaleb Kohn <kohnkaleb@gmail.com>
2023-03-25 19:34:30 -0500

    chess opening quiz using AbChess api

---
+++ OpeningPractice.js
@@ -0,0 +1,9 @@
+import {AbChess} from ".AbChess-0.3.0"
+
+var abChess = {};
+        var options = {
+            animated: false
+        };
+        
+abChess = new AbChess("chessboard", options);
+abChess.setFEN();
